fead4a016b672fa56c92dc4234e0ab3560013088
Yaguchan <guchi4867@gmail.com>
2024-07-21 20:26:18 +0900

    20240721

--- dataset.py
+++ dataset.py
@@ -6,24 +6,7 @@ from PIL import Image
 from torch.utils.data import Dataset
 
 
-# DataAugmentation
-def inflated_image(img, flip=True, thr=True, filt=True, resize=True, erode=True):
-    methods = [flip, thr, filt, resize, erode]
-    img_size = img.shape
-    mat = cv2.getRotationMatrix2D(tuple(np.array([img_size[1], img_size[0]]) / 2 ), 45, 1.0)
-    filter1 = np.ones((3, 3))
-    images = [img]
-    scratch = np.array([
-        lambda x: cv2.flip(x, 1),                                                                           # 左右反転
-        lambda x: cv2.threshold(x, 100, 255, cv2.THRESH_TOZERO)[1],                                         # 閾値処理
-        lambda x: cv2.GaussianBlur(x, (5, 5), 0),                                                           # ぼかし
-        lambda x: cv2.resize(cv2.resize(x, (img_size[1]//6, img_size[0]//6)), (img_size[1], img_size[0])),  # モザイク処理
-        lambda x: cv2.erode(x, filter1)                                                                     # 収縮
-    ])
-    doubling_images = lambda f, imag: (imag + [f(i) for i in imag])
-    for func in scratch[methods]:
-        images = doubling_images(func, images)
-    return images
+sizes = ['base', '32', '64', '128']
 
 
 class IdolData(Dataset):
@@ -51,9 +34,14 @@ class IdolData(Dataset):
                 else:
                     aug_img_paths = [os.path.join(aug_dir, class_name, img_name) for img_name in img_names if img_name != '.DS_Store']
                     for aug_img_path in aug_img_paths[:train_size]:
+                        for size in sizes:
+                            self.images.append(aug_img_path.replace('.jpg', f'_{size}.jpg'))
+                        """
                         for i in range(32):
                             self.images.append(aug_img_path.replace('.jpg', f'_{i}.jpg'))
                         self.labels.extend([self.class_to_idx[class_name]]*32)
+                        """
+                        self.labels.extend([self.class_to_idx[class_name]]*len(sizes))
                         
             elif split == 'val':
                 self.images.extend(img_paths[train_size:train_size+val_size])
