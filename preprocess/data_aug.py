69717eba0a30c87d1e193762c05d9bc90b0db22a
Yaguchan <guchi4867@gmail.com>
2024-08-07 08:17:16 +0900

    20240807

--- preprocess/data_aug.py
+++ preprocess/data_aug.py
@@ -4,21 +4,27 @@ import cv2
 import random
 import numpy as np
 from tqdm import tqdm
-from PIL import Image, ImageEnhance, ImageOps
+from PIL import Image, ImageEnhance, ImageOps, ImageFilter
 
 
 # python preprocess/data_aug.py
 INDIR = './data/twitter/images/face'
-OUTDIR = './data/twitter_aug4/images/face'
+OUTDIR = './data/twitter_aug10/images/face'
 
 
-# 1. 回転 (Rotation)
+# aug 0-11
+## 0.フリップ (Flip)
+def flip(image):
+    flipped_image = ImageOps.mirror(image)
+    return flipped_image
+
+## 1.回転 (Rotation)
 def rotation(image):
     angle = random.uniform(-15, 15)
     rotated_image = image.rotate(angle)
     return rotated_image
 
-# 2. 平行移動 (Translation)
+## 2.平行移動 (Translation)
 def translation(image):
     width, height = image.size
     delta = min(width, height) // 4
@@ -27,19 +33,14 @@ def translation(image):
     translated_image = image.transform((width, height), Image.AFFINE, M)
     return translated_image
 
-# 3. スケーリング (Scaling)
+## 3.スケーリング (Scaling)
 def scaling(image, min_size=20):
     min_scale = min_size / min(image.width, image.height)
     scale_factor = random.uniform(min_scale, 1.0)
     scaled_image = image.resize((int(image.width * scale_factor), int(image.height * scale_factor)))
     return scaled_image
 
-# 4. フリップ (Flip)
-def flip(image):
-    flipped_image = ImageOps.mirror(image)
-    return flipped_image
-
-# 5. ノイズの追加 (Adding Noise)
+## 4.ノイズの追加 (Adding Noise)
 def adding_noise(image):
     image_array = np.array(image)
     noise = np.random.normal(0, 25, image_array.shape).astype(np.uint8)
@@ -48,13 +49,13 @@ def adding_noise(image):
     noisy_image = Image.fromarray(noisy_image_array)
     return noisy_image
 
-# 6. カラージッタリング (Color Jittering)
+## 5.カラージッタリング (Color Jittering)
 def color_jittering(image):
     color_enhancer = ImageEnhance.Color(image)
     color_jittered_image = color_enhancer.enhance(random.uniform(0.7, 1.3))
     return color_jittered_image
 
-# 7. カットアウト (Cutout)
+## 6.カットアウト (Cutout)
 def cutout(image):
     cutout_size = min(image.width, image.height) // 4 # (30, 30)
     x = random.randint(0, image.width - cutout_size)
@@ -64,45 +65,60 @@ def cutout(image):
     cutout_image.paste(cutout_area, (x, y))
     return cutout_image
 
+## 7.ガウシアンぼかし（Gaussian Blur）
+def gaussian_blur(image):
+    blurred_image = image.filter(ImageFilter.GaussianBlur(radius=random.uniform(0.1, 2.0)))
+    return blurred_image
+
+## 8.明度変化 (Brightness Adjustment)
+def brightness_adjustment(image):
+    brightness_enhancer = ImageEnhance.Brightness(image)
+    brightness_adjusted_image = brightness_enhancer.enhance(random.uniform(0.7, 1.3))
+    return brightness_adjusted_image
+
+## 9.コントラスト調整 (Contrast Adjustment)
+def contrast_adjustment(image):
+    contrast_enhancer = ImageEnhance.Contrast(image)
+    contrast_adjusted_image = contrast_enhancer.enhance(random.uniform(0.7, 1.3))
+    return contrast_adjusted_image
+
+## 10.シャープ化 (Sharpening)
+def sharpening(image):
+    sharpness_enhancer = ImageEnhance.Sharpness(image)
+    sharpened_image = sharpness_enhancer.enhance(random.uniform(1.0, 2.0))
+    return sharpened_image
+
 
 # num -> aug_function
 aug_functions = {
-    0: rotation,
-    1: translation,
-    2: scaling,
-    3: flip,
+    0: flip,
+    1: rotation,
+    2: translation,
+    3: scaling,
     4: adding_noise,
     5: color_jittering,
-    6: cutout
+    6: cutout,
+    # 7: gaussian_blur,
+    # 8: brightness_adjustment,
+    # 9: contrast_adjustment,
+    # 10: sharpening,
 }
-# データ拡張1つ
-def augment_image1(image_path, auglen=7):
+def augment_image(image_path, auglen=7, times=10):
     aug_images = []
-    for i in range(auglen):
-        image = Image.open(image_path)
-        aug_images.append(aug_functions[i](image))
-    return aug_images
-# データ拡張2つ
-def augment_image2(image_path, auglen=7):
-    aug_images = []
-    for i in range(auglen):
-        for j in range(i+1, auglen):
-            list_num = [i, j]
+    # normal image
+    image = Image.open(image_path)
+    aug_images.append(image)
+    # aug 0
+    image = aug_functions[0](image)
+    aug_images.append(image)
+    # aug 1-6
+    for i in range(times):
+        for j in range(1, auglen):
             image = Image.open(image_path)
-            for num in list_num:
-                image = aug_functions[num](image)
+            image = aug_functions[j](image)
             aug_images.append(image)
     return aug_images
-# データ拡張複数回
-def augment_image3(image_path, auglen=7, times=5):
-    aug_images = []
-    for i in range(auglen):
-        for j in range(times):
-            image = Image.open(image_path)
-            image = aug_functions[i](image)
-            aug_images.append(image)
-            if i == 3: break
-    return aug_images
+
 
 def main():
     
@@ -119,7 +135,7 @@ def main():
         img_names = [img_name for img_name in img_names if img_name != '.DS_Store']
         for img_name in img_names:
             img_path = os.path.join(in_member_dir, img_name)
-            inflated_images = augment_image3(img_path)
+            inflated_images = augment_image(img_path)
             for i, im in enumerate(inflated_images):
                 im.save(os.path.join(out_member_dir, img_name.replace('.jpg', f'_{i}.jpg')))
 
